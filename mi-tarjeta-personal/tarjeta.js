5d4983598d1fc8a49f3a209aca205b8e50420478
Lana <ksona6417@gmail.com>
2025-10-27 22:56:50 +0100

    Añado nueva carpeta

---
+++ mi-tarjeta-personal/tarjeta.js
@@ -0,0 +1,6 @@
+import colors from 'colors';
+
+console.log(colors.yellow('Ilona'));
+console.log(colors.cyan('Torrent'));
+console.log(colors.green('Quero aprender programación porque es interesante y útil'));
+console.log(colors.rainbow('Tu puedes todo que queres🩵'));
